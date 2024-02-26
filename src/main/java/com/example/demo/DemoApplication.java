e12cafcfa1a1472c74bede1d46eaa56bfd7e86b0
Ernar <230152@astanait.edu.kz>
2024-02-26 14:00:49 +0600

    Library connected.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	@GetMapping
+	public Library.Users.Admin hello(){
+		return new Library.Users.Admin("Krmall50", "Ernar@mail", "87559992955");
 	}
 
 }
